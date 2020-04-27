be8701aef52c17d026628f07f3ce44a2545252de
Christopher Basketfield <chris.basketfield@gmail.com>
2020-04-26 16:50:20 -0800

    import component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ToDos from './components/ToDos.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <ToDos />
     </div>
   );
 }
---
+++ src/components/ToDos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToDos() {
+  return (
+    <div>
+      <h1>ToDo</h1>
+    </div>
+  );
+}
+
+export default ToDos;
